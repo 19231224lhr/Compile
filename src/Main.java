a0a0246e6e12a95b96e4ac4ce1aa23ba38db178c
19231224lhr <1836080103@qq.com>
2021-10-25 21:14:42 +0800

    修改输入输出重定向错误

--- src/Main.java
+++ src/Main.java
@@ -9,40 +9,40 @@ public class Main {
     public static void main(String[] args) {
          // System.out.println("Start!");
         // BailErrorStrategy bailErrorStrategy = new BailErrorStrategy();
-//        String filePath = args[0];
-//        String OutputPath = args[1];
-//        File file = new File(filePath);
-//        InputStreamReader read = null;
-//        try {
-//            read = new InputStreamReader(
-//                    new FileInputStream(file));
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        BufferedReader bufferedReader = new BufferedReader(read);
-//        String input_1 = null;
-//        StringBuilder input = new StringBuilder();
-//        while (true) {
-//            try {
-//                if ((input_1 = bufferedReader.readLine()) == null) break;
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            input.append(input_1 + "\n");
-//            // System.out.println(input_1);
-//        }
-//        System.out.println(input);
-//        try {
-//            System.setOut(new PrintStream(new BufferedOutputStream(
-//                    new FileOutputStream(OutputPath)), true));
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//
-//        }
+        String filePath = args[0];
+        String OutputPath = args[1];
+        File file = new File(filePath);
+        InputStreamReader read = null;
+        try {
+            read = new InputStreamReader(
+                    new FileInputStream(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader bufferedReader = new BufferedReader(read);
+        String input_1 = null;
+        StringBuilder input = new StringBuilder();
+        while (true) {
+            try {
+                if ((input_1 = bufferedReader.readLine()) == null) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            input.append(input_1 + "\n");
+            // System.out.println(input_1);
+        }
+        System.out.println(input);
+        try {
+            System.setOut(new PrintStream(new BufferedOutputStream(
+                    new FileOutputStream(OutputPath)), true));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
 
-        String input = "int main() {\n" +
-                "    return -+(+-((-+(-+(1)))));\n" +
-                "}";
+        }
+
+//        String input = "int main() {\n" +
+//                "    return -+(+-((-+(-+(1)))));\n" +
+//                "}";
         CharStream inputStream = CharStreams.fromString(input.toString());
         testLexer lexer = new testLexer(inputStream);
         CommonTokenStream tokenStream = new CommonTokenStream(lexer);
